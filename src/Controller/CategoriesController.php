730015755b8f7c9a3f89c9059295df65caebc70a
Mary <marydiva201170@gmail.com>
2023-11-13 20:02:50 +0100

    notif

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Entity\Categorie;
+use App\Repository\ProduitRepository;
 use App\Repository\CategorieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +16,14 @@ class CategoriesController extends AbstractController
 
 {
     #[Route('/{sousrubriqueart}/{id}', name: 'list')]
-    public function list(Categorie $categorie, CategorieRepository $categorieRepository): Response
+    public function list(Categorie $categorie, CategorieRepository $categorieRepository, ProduitRepository $produitRepository, Produit $produit, $id): Response
     {
         return $this->render('categories/list.html.twig', [
             'categories' => $categorieRepository->findAll($categorie->getId()),
+            'categorie' => $categorie,
+            'produits' => $produit,
+            'produits' => $produitRepository->findBy(['categorie'=>$id]),
+
         ]);
     }
 
