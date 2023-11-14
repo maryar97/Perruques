167420ddc7b5dc7624a25331bebcdebc81d4c77c
Mary <marydiva201170@gmail.com>
2023-11-14 10:49:19 +0100

    notif

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -15,6 +15,7 @@ class AccueilController extends AbstractController
     { 
                 return $this->render('accueil/index.html.twig', [
             'categories'=>$categorieRepository->findAll(),
+            
         ]);
     }
 
