787b23ed4e26d67170a9a593e7c703319678b367
Mary <marydiva201170@gmail.com>
2023-11-16 13:09:57 +0100

    notif

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UsersRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/utilisateurs', name: 'admin_users_')]
 class UsersController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
-{
-    return $this->render('admin/users/index.html.twig');
-}
+    public function index(UsersRepository $usersRepository): Response
+    {
+    $users = $usersRepository->findBy([], ['prenom' => 'asc']);
+    return $this->render('admin/users/index.html.twig', compact('users'));
+    }
 }
