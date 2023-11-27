9abc6c1797a7a4e07cc982ce05ecb29e9bab27e9
Mary <marydiva201170@gmail.com>
2023-11-27 11:50:39 +0100

    notif

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -33,39 +33,89 @@ class PaymentController extends AbstractController
     {
 
         $produitStripe = []; 
+        $total=0;
 
         $commande = $this->em->getRepository(Commande::class)->findOneBy(['reference' => $reference]);
 
         if(!$commande){
             return $this->redirectToRoute('cart_index');
         }
+        
 
         foreach($commande->getRecapDetails()->getValues() as $produit){
+            
+            $pttc=0;
+            $total1=0; 
+            $Facturetotaltva = 20;  
+
             $produitData = $this->em->getRepository(Produit::class)->findOneBy(['sousrubriqueart' => $produit->getProduit()]);
             $produitStripe[] = [
                 'price_data' => [
                     'currency' => 'eur',
-                    'unit_amount' => $produitData->getPrixachat(),
+                    'unit_amount' => $produitData->getPrixachat() * 100,
                     'product_data' => [
                         'name' => $produit->getProduit()
                     ]
                 ], 
-                'quantite' => $produit->getQuantite(),
+                'quantity' => $produit->getQuantite()
             ];
-
+        
+            $total1 += $produit->getPrixachat() *  $produit->getQuantite();
         }
-        // dd($commande);
 
-        $produitStripe = [
+        $produitStripe[] = [
             'price_data' => [
                 'currency' => 'eur',
-                'unit_amount' => $commande->getTransporteurPrix(),
+                'unit_amount' => (round(($total1 * 0.2),2)) * 100,
+                'product_data' => [
+                    'name' => "TVA"
+                ]
+                ],
+                'quantity' => 1,
+            ];
+
+
+
+            
+         // dd($produitStripe);
+
+        $produitStripe[] = [
+            'price_data' => [
+                'currency' => 'eur',
+                'unit_amount' => $commande->getTransporteurPrix() * 100,
                 'product_data' => [
                     'name' => $commande->getTransporteurNom()
                 ]
             ], 
             'quantity' => 1,
         ];
+
+    //     if($pttc > 200) {
+    //         //$total =$totaltva+0; 
+    //         $produitStripe[] = [
+    //                 'price_data' => [
+    //                     'currency' => 'eur',
+    //                     'unit_amount' => 0,
+    //                     'product_data' => [
+    //                         'name' => 'fdp'
+    //                     ]
+    //                     ],
+    //                     'quantity' => 1,
+    //                 ];
+    //     }
+    //     else {
+    //         $produitStripe[] = [
+    //             'price_data' => [
+    //                 'currency' => 'eur',
+    //                 'unit_amount' => $fdp * 100,
+    //                 'product_data' => [
+    //                     'name' => 'fdp'
+    //                 ]
+    //                 ],
+    //                 'quantity' => 1,
+    //             ];
+        
+    // }
 
         Stripe::setApiKey('sk_test_51O9QyeJcB7aIs6zZrucNH3s5gBlnUgquUXkR0KmrRdBGd3lVjWdp2jRc1OzsLGoj5LA5y1DISPIdT8pADgrT0DKX00qdskr1kk');
 
